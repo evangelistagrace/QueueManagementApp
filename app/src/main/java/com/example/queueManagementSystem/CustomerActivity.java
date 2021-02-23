a21886c8af9675cd75a7a32ea1754eeffb24282c
Grace <evangrace98@gmail.com>
2021-02-23 23:02:35 +0800

    add queue simulator logic and partially link it with app

--- app/src/main/java/com/example/queueManagementSystem/CustomerActivity.java
+++ app/src/main/java/com/example/queueManagementSystem/CustomerActivity.java
@@ -9,11 +9,18 @@ import androidx.fragment.app.FragmentTransaction;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+
 public class CustomerActivity extends AppCompatActivity {
     private ActionBar toolbar;
+    protected static ArrayList<Service> services = new ArrayList<>();
+    Button btnServiceLoans, btnServiceAccounts, btnServicePayments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +28,34 @@ public class CustomerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_activity);
 
+        // add code after this line
+        btnServiceLoans = findViewById(R.id.btnServiceLoans);
+        btnServiceAccounts = findViewById(R.id.btnServiceAccounts);
+
         toolbar = getSupportActionBar();
-        toolbar.setTitle("Home");
+//        toolbar.setTitle("Home");
+
+        // start background processes
+        services.add(new Service(1, "Service 1"));
+        services.add(new Service(2, "Service 2"));
+        for (Service service: services) {
+            service.startCounters();
+        }
+
+       btnServiceLoans.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Customer customer = new Customer("grace");
+               customer.sendTicketRequest(customer, 1); //assume customer had clicked on a service with service id 1
+               Toast.makeText(CustomerActivity.this, "new ticket requested!", Toast.LENGTH_SHORT).show();
+           }
+       });
+
+//        Customer customer = new Customer("grace");
+//        Customer customer2 = new Customer("anna");
+//        customer.sendTicketRequest(customer, 1); //assume customer had clicked on a service with service id 1
+//        customer2.sendTicketRequest(customer2, 1);
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
@@ -32,16 +65,16 @@ public class CustomerActivity extends AppCompatActivity {
                 Fragment fragment;
                 switch (item.getItemId()) {
                     case R.id.action_home:
-                        toolbar.setTitle("Home");
+//                        toolbar.setTitle("Home");
                         getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
                         return true;
                     case R.id.action_tickets:
-                        toolbar.setTitle("Tickets");
+//                        toolbar.setTitle("Tickets");
                         fragment = new CustomerTicketFragment();
                         loadFragment(fragment);
                         return true;
                     case R.id.action_profile:
-                        toolbar.setTitle("Profile");
+//                        toolbar.setTitle("Profile");
                         fragment = new CustomerProfileFragment();
                         loadFragment(fragment);
                         return true;
@@ -49,6 +82,10 @@ public class CustomerActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    public static ArrayList<Service> getServices() {
+        return services;
     }
 
     private void loadFragment(Fragment fragment) {
---
+++ app/src/main/java/com/example/queueManagementSystem/QueueManager.java
@@ -0,0 +1,158 @@
+package com.example.queueManagementSystem;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class QueueManager {
+    private int servingTicketId;
+    private Queue<Ticket> tickets = new LinkedList<>();
+    private long lastTicketNum = 0;
+    Timer newTimer;
+    Counter counter;
+    Queue<Customer> queuedCustomers = new LinkedList<>();
+
+    public QueueManager(Counter counter) {
+        this.counter = counter;
+    }
+
+    public void generateTickets() {
+        int randomQNum = this.getRandomQueueNumber();
+        for (int i=0; i<randomQNum; i++) {
+            Ticket ticket = generateTicket();
+            this.tickets.add(ticket);
+        }
+    }
+
+    public Ticket generateTicket() {
+        int counterNum = this.counter.getId();
+        long lastTicketNumber = getLastTicketNumber();
+        Long ticketNum = Long.parseLong(String.valueOf(counterNum) + String.valueOf(lastTicketNumber));
+        return new Ticket(111, counterNum, ticketNum); //ticket created with random time interval assigned
+    }
+
+    public int getRandomQueueNumber() {
+        int min = 1;
+        int max = 10;
+        int randomQueueNumber = (int)(Math.random() * (max - min + 1) + min);
+        System.out.println(randomQueueNumber);
+        return randomQueueNumber;
+    }
+
+    public long getLastTicketNumber() {
+        return ++lastTicketNum;
+    }
+
+    public Queue<Ticket> getTickets() {
+        return this.tickets;
+    }
+
+    public void getNextTicket() {
+        QueueManager queueManager = this;
+        Ticket nextTicket = this.tickets.peek();
+        long period = (long)(nextTicket.getTicketTimeInterval() * 1000);
+
+        newTimer = new Timer();
+        newTimer.schedule(new TicketHelper(queueManager), 0, period); //start ticket serving scheduler
+    }
+
+    class TicketHelper extends TimerTask {
+        public QueueManager queueManager;
+        public Queue<Ticket> tickets;
+        Customer customer = null;
+
+        public TicketHelper(QueueManager queueManager) {
+            this.queueManager = queueManager;
+            this.tickets = queueManager.getTickets();
+        }
+
+        public void run() {
+            Ticket ticket = this.tickets.peek();
+            assert ticket != null;
+            long period = (long)(ticket.getTicketTimeInterval() * 1000);
+
+            Timer oldTimer;
+            if (!ticket.isExpired()) {
+
+                //serve immediately
+                for (Customer queuedCustomer: this.queueManager.queuedCustomers) {
+                    if (queuedCustomer.getTicket().getTicketNumber() == ticket.getTicketNumber()) {
+                        customer = queuedCustomer;
+                    }
+                }
+
+                if (customer != null) {
+                    System.out.println("Currently serving customer " + customer.getUsername() + " with " + ticket.getTicketNumber());
+                } else {
+                    System.out.println("Currently serving ticket" + ticket.getTicketNumber());
+                }
+
+                // DELAY THREAD TO SIMULATE RANDOM SERVING PERIOD //
+                try {
+                    Thread.sleep(period);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                }
+
+                // CONTINUE THREAD EXECUTION //
+
+                if (customer != null) {
+                    System.out.println("Finished serving customer " + customer.getUsername() + " with " + ticket.getTicketNumber());
+                    //remove customer from queue
+                    customer.setInQueue(false);
+                    customer.setTicket(null);
+                    queuedCustomers.poll();
+                } else {
+                    System.out.println("Finished serving " + ticket.getTicketNumber());
+                }
+                ticket.setExpired(true);
+
+            } else {
+                oldTimer = newTimer; //assign previous timer as oldtimer so it can be cancelled and purge after new timer starts
+
+                this.tickets.poll(); //remove served ticket
+
+                if (this.tickets.peek() != null) { //check for remaining tickets
+                    newTimer = new Timer(); //instantiate a new timer
+                    newTimer.schedule(new TicketHelper(this.queueManager), 0, period);
+                } else { //no more tickets left
+                    System.out.println("~~ Finished serving all tickets for counter " + this.queueManager.counter.getId() + "~~");
+                    this.queueManager.run(); //gen new batch of tickets
+                }
+
+                oldTimer.cancel();
+                oldTimer.purge();
+            }
+        }
+    }
+
+    // process that keeps on running
+    public void run() {
+        this.generateTickets();
+        System.out.println("~~Generated new batch of tickets for counter " + this.counter.getId() + " ~~");
+        for (Ticket ticket : tickets) {
+            System.out.println("Ticket number: " + ticket.getTicketNumber() + " with time interval: " + ticket.getRandomTimeInterval());
+        }
+        this.getNextTicket();
+    }
+
+    // handle ticket request from customer
+    public void handleTicketRequest(Customer customer) {
+        Ticket newTicket = this.generateTicket();
+        System.out.println("QUEUED CUSTOMER " + customer.getUsername() + " WITH TICKET NUMBER " + newTicket.getTicketNumber());
+        customer.setTicket(newTicket);
+        customer.setInQueue(true);
+        this.tickets.add(newTicket);
+        queuedCustomers.add(customer);
+    }
+
+    public int getServingTicketId() {
+        return servingTicketId;
+    }
+
+    public void setServingTicketId(int servingTicketId) {
+        this.servingTicketId = servingTicketId;
+    }
+
+}
